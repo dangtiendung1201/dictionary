ae54bb64b3983e9a38cd90b0fa2e5e6b3cef23ad
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-09-30 08:27:46 +0700

    feat: Create command line version's structure

---
+++ dictionary/src/DictionaryCommandLine.java
@@ -0,0 +1,20 @@
+public class DictionaryCommandLine extends DictionaryManagement{
+        // Show all words alphabetically.
+        public static void showAllWords() {
+
+        }
+
+        // Call insertFromCommandline() function from DictionaryManagement and showAllWords()
+        public static void dictionaryBasic() {
+
+        }
+
+        //
+        public static void dictionaryAdvanced() {
+
+        }
+
+        public static void main(String[] args){
+
+        }
+}
---
+++ dictionary/src/DictionaryManagement.java
@@ -0,0 +1,13 @@
+public class DictionaryManagement {
+    private ArrayList<Word> dictionary;
+    private Trie;
+
+    DictionaryManagement() {
+        dictionary = new ArrayList<> ();
+        Trie = new Trie();
+    }
+    public void insertFromFile() {
+
+    }
+    
+}
