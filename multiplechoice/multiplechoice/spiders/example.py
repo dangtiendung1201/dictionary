644c5c58017b373ff2d3b72777dd89daffd77793
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-10-06 18:51:45 +0000

    feat: Finish  crawler for multiple choice

--- multiplechoice/multiplechoice/spiders/example.py
+++ multiplechoice/multiplechoice/spiders/example.py
@@ -2,9 +2,33 @@ import scrapy
 
 
 class ExampleSpider(scrapy.Spider):
+    words = []
+
+    with open('wordList.txt', 'r') as f:
+        for line in f:
+            words.append(line.strip())
+
     name = "example"
     allowed_domains = ["sentence.yourdictionary.com"]
-    start_urls = ["https://sentence.yourdictionary.com"]
+    start_urls = ["https://sentence.yourdictionary.com/" + word for word in words]
 
     def parse(self, response):
-        pass
+        # englishWord
+        englishWord = response.url.split("/")[-1]
+        # example
+        exampleTemp = response.xpath('//div[@class="sentence-item"]/descendant-or-self::*/text()').getall()
+        example = ""
+
+        for i in range(len(exampleTemp)):
+            if exampleTemp[i] == " ":
+                break
+            else:
+                example += exampleTemp[i]
+
+        yield {
+            englishWord: {
+                'example': example
+            }
+        }
+
+
