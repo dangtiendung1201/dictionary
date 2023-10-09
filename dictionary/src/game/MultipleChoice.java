a73a9e064365c59b8ea1ed2ba02a06b93a519e96
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-10-09 03:47:02 +0000

    feat: Read file and generate question for Multiple Choice game

--- dictionary/src/game/MultipleChoice.java
+++ dictionary/src/game/MultipleChoice.java
@@ -4,13 +4,18 @@ import java.nio.file.Files;
 
 import word.Word;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class MultipleChoice extends GameManagement {
-    private List<String> dataFile;
+    private static int maxQuestion = 33482;
+
+    private ArrayList<Question> dataFile = new ArrayList<Question>();
     private State state;
     private Question question;
 
@@ -19,18 +24,41 @@ public class MultipleChoice extends GameManagement {
         state = State.PLAYING;
     }
 
-    private List<String> getDataFromFile() {
+    private void getDataFromFile() {
         System.out.println("Reading data from file...");
-        
+
+        // try {
+        // return
+        // Files.readAllLines(Paths.get("dictionary/data/MultipleChoiceData.txt"));
+        // } catch (IOException e) {
+        // System.out.println("Error: " + e);
+        // return null;
+        // }
         try {
-            return Files.readAllLines(Paths.get("dictionary/data/MultipleChoiceData.txt"));
+            Scanner sc = new Scanner(new File("dictionary/data/MultipleChoiceData.txt"));
+            while (sc.hasNext()) {
+                String tmp[] = sc.nextLine().split("\t");
+                dataFile.add(new Question(tmp[0], tmp[1]));
+            }
+            sc.close();
         } catch (IOException e) {
             System.out.println("Error: " + e);
-            return null;
         }
     }
 
+    private void generateQuestion() {
+        Random rand = new Random();
+        int index = rand.nextInt(maxQuestion);
+        question = dataFile.get(index);
+    }
+
     public void start() {
+        System.out.println("You are playing Multiple Choice Game");
         getDataFromFile();
+        
+        while (state == State.PLAYING)
+        {
+            generateQuestion();
+        }
     }
 }
