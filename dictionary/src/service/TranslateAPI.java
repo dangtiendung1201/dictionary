cc4e7b5d23d3c26fd6812cfc60b2f83151e42c6e
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-10-29 10:26:33 +0700

    fix: Set timeout for API and handle special character in returned JSON

--- dictionary/src/service/TranslateAPI.java
+++ dictionary/src/service/TranslateAPI.java
@@ -1,7 +1,10 @@
 package service;
 
 import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import com.google.gson.*;
 import okhttp3.OkHttpClient;
@@ -12,9 +15,13 @@ import okhttp3.Response;
 public class TranslateAPI extends Service {
     private static String key = "66380cc580084f81aa83c321e37fe0d0";
     private static String location = "eastasia";
+    private static int timeout = 10000; // Milliseconds
 
-    // Instantiates the OkHttpClient.
-    OkHttpClient client = new OkHttpClient();
+    // Instantiates the OkHttpClient and set timeouts.
+
+    OkHttpClient client = new OkHttpClient().newBuilder().connectTimeout(timeout, TimeUnit.MILLISECONDS)
+            .readTimeout(timeout, TimeUnit.MILLISECONDS)
+            .writeTimeout(timeout, TimeUnit.MILLISECONDS).build();
 
     // This function performs a POST request.
     private String Post(String sentence, String originalLanguage, String translatedLanguage) throws IOException {
@@ -25,19 +32,28 @@ public class TranslateAPI extends Service {
                         + "&to=" + translatedLanguage)
                 .post(body)
                 .addHeader("Ocp-Apim-Subscription-Key", key)
-                // location required if you're using a multi-service or regional (not global) resource. 
-                .addHeader("Ocp-Apim-Subscription-Region", location) 
+                // location required if you're using a multi-service or regional (not global)
+                // resource.
+                .addHeader("Ocp-Apim-Subscription-Region", location)
                 .addHeader("Content-type", "application/json")
                 .build();
-        Response response = client.newCall(request).execute();
-        return response.body().string();
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (SocketTimeoutException e) {
+            System.out.println("Request timed out.");
+            return "Ấn chậm thôi bạn êi.";
+        } catch (InterruptedIOException e) {
+            System.out.println("Request interrupted.");
+            return "Ấn chậm thôi bạn êi.";
+        }
     }
 
-    // This function prettifies the json response.
     private static String prettify(String json_text) {
         JsonElement json = JsonParser.parseString(json_text);
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String result = gson.toJson(json.getAsJsonArray().get(0).getAsJsonObject().get("translations").getAsJsonArray().get(0).getAsJsonObject().get("text"));
+        Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
+        String result = gson.toJson(json.getAsJsonArray().get(0).getAsJsonObject().get("translations").getAsJsonArray()
+                .get(0).getAsJsonObject().get("text"));
         return result.substring(1, result.length() - 1);
     }
 
