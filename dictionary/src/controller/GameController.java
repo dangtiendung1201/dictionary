83e22b12327d7b375fff047f437f3be055f3b465
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-11-11 19:11:16 +0700

    feat: Create Game controller

---
+++ dictionary/src/controller/GameController.java
@@ -0,0 +1,69 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.util.Duration;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.AnchorPane;
+
+public class GameController extends Controller {
+    @FXML
+    private AnchorPane gamePane;
+    @FXML
+    private Button hangmanBtn, mcBtn, gtwBtn;
+    @FXML
+    private Tooltip hangmanBtnTip, mcBtnTip, gtwBtnTip;
+
+    private void setNode(Node node) {
+        gamePane.getChildren().clear();
+        gamePane.getChildren().add(node);
+    }
+
+    private void showPane(String path) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource(path));
+            setNode(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void initialize() {
+        hangmanBtnTip.setShowDelay(Duration.seconds(0.5));
+        mcBtnTip.setShowDelay(Duration.seconds(0.5));
+        gtwBtnTip.setShowDelay(Duration.seconds(0.5));
+
+        hangmanBtn.setOnAction(actionEvent -> {
+            try {
+                // showPane("/view/HangmanUI.fxml");
+                System.out.println("Hangman");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        mcBtn.setOnAction(actionEvent -> {
+            try {
+                // showPane("/view/MultipleChoiceUI.fxml");
+                System.out.println("Multiple Choice");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        gtwBtn.setOnAction(actionEvent -> {
+            try {
+                // showPane("/view/GuessTheWordUI.fxml");
+                System.out.println("Guess The Word");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+}
