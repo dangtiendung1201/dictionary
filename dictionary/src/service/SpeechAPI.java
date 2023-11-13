44bf18ca54ac95bd0c9d3ce443220e54d08d0702
ttb06 <112877189+ttb06@users.noreply.github.com>
2023-11-13 21:48:47 +0700

    add Exception for SpeechAPI

--- dictionary/src/service/SpeechAPI.java
+++ dictionary/src/service/SpeechAPI.java
@@ -3,39 +3,30 @@ package service;
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
-import java.util.Locale;
-import javax.speech.Central;
-import javax.speech.synthesis.Synthesizer;
-import javax.speech.synthesis.SynthesizerModeDesc;
+import java.util.Scanner;
 
 public class SpeechAPI extends Service {
 
-    public void speak(String text) throws Exception {
+    public static void main(String[] args) {
+        SpeechAPI speech = new SpeechAPI();
+        System.out.println("Enter a sentence you want to speak: ");
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
         try {
-            System.setProperty(
-                    "freetts.voices",
-                    "com.sun.speech.freetts.en.us"
-                            + ".cmu_us_kal.KevinVoiceDirectory");
-
-            Voice voice = VoiceManager.getInstance().getVoice("kevin16");
-            voice.allocate();
-            voice.speak(text);
+            speech.speak(sentence);
         } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("ngu vl");
+            System.out.println(e);
         }
     }
 
-    public static void main(String[] args) {
-        SpeechAPI speech = new SpeechAPI();
-//        Voice
-        int ntime = 3;
-        while (ntime-- != 0) {
-            try {
-                speech.speak("How?");
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-        }
+    public void speak(String text) throws Exception {
+        System.setProperty(
+                "freetts.voices",
+                "com.sun.speech.freetts.en.us"
+                        + ".cmu_us_kal.KevinVoiceDirectory");
+
+        Voice voice = VoiceManager.getInstance().getVoice("kevin16");
+        voice.allocate();
+        voice.speak(text);
     }
 }
