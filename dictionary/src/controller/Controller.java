cd27a6bdfebde08e9fb6b253a5babb17ab2d6302
Đặng Nguyễn Duy Trúc <truc12a2cvp@gmail.com>
2023-11-26 20:21:46 +0700

    add back up data

--- dictionary/src/controller/Controller.java
+++ dictionary/src/controller/Controller.java
@@ -1,16 +1,23 @@
 package controller;
 
 import management.DictionaryManagement;
+import trie.exception.AddWordException;
+
 public class Controller {
     static protected DictionaryManagement management;
     static {
         management = new DictionaryManagement();
         try {
             management.insertFromFile("dictionary/resourses/data/merged.txt");
+        } catch (AddWordException e) {
+            System.out.println(e.getMessage());
+            management.insertFromFile("dictionary/resourses/data/merged-backup.txt");
+        }
+
+        try {
             management.insertMyWordListFromFile("dictionary/resourses/data/myList.txt");
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
